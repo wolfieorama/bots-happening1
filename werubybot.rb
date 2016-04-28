517f52ac086a2d872a6d70a02039ff5a8a4d66db
wolfieorama <wachirawamunyi@gmail.com>
2016-04-29 00:37:46 +0300

    added a send contact method

--- werubybot.rb
+++ werubybot.rb
@@ -6,11 +6,13 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "I am the SitePoint bot, My commands are /sitepoint /map")
-    when '/sitepoint'
-      bot.api.send_message(chat_id: message.chat.id, text: "welcometo http://sitepoint.com")
-    when '/map'
-      bot.api.send_location(chat_id: message.chat.id, latitude: -37.807416, longitude: 144.985339)
+      bot.api.send_message(chat_id: message.chat.id, text: "I am the weruby all day all night bot use /lang, /frameworks and /callrubyist to know more")
+    when '/lang'
+      bot.api.send_message(chat_id: message.chat.id, text: "Yes the language is ruby https://www.ruby-lang.org")
+    when '/frameworks'
+      bot.api.send_message(chat_id: message.chat.id, text: "my frameworks include Rails, Sinatra, Cuba etc..")
+    when '/callrubyist'
+      bot.api.send_contact(chat_id: message.chat.id, phone_number: "0722558899", first_name: "Yukihiro", last_name: "Matsumoto", user_id: 1)
     end
   end
 end
