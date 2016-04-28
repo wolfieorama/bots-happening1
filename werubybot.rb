00316c9da4a9ad57b2b0885e7b4e08857169c3c5
wolfieorama <wachirawamunyi@gmail.com>
2016-04-29 00:00:18 +0300

    added more options and commands

--- werubybot.rb
+++ werubybot.rb
@@ -1,12 +1,16 @@
 require 'telegram/bot'
 
-token = 'your token'
+token = 'API'
 
 Telegram::Bot::Client.run(token) do |bot|
- bot.listen do |message|
-  case message.text
-  when '/sitepoint'
-   bot.api.send_message(chat_id: message.chat.id, text: "welcometo http://sitepoint.com")
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "I am the SitePoint bot, My commands are /sitepoint /map")
+    when '/sitepoint'
+      bot.api.send_message(chat_id: message.chat.id, text: "welcometo http://sitepoint.com")
+    when '/map'
+      bot.api.send_location(chat_id: message.chat.id, latitude: -37.807416, longitude: 144.985339)
+    end
   end
- end
 end
