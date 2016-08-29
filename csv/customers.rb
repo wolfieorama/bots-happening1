d2902333cfe1c68f4bc495f92db080f4b42120c4
wolfieorama <wachirawamunyi@gmail.com>
2016-08-29 16:45:13 +0300

    addded header

--- csv/customers.rb
+++ csv/customers.rb
@@ -1,20 +1,20 @@
 require 'csv'
-customers = CSV.read('example.csv')
+customers = CSV.read('example.csv', headers: true)
 average_money_spent = Array.new
-CSV.foreach('example.csv', converters: :numeric) do |row|
-  average_money_spent << row[2] / row[1]
-end
-
-customers.each do |customer|
- customer << average_money_spent.shift
-end
-
-# customers.each do |row|
-#   puts row.inspect
+# CSV.foreach('example.csv', converters: :numeric) do |row|
+#   average_money_spent << row[2] / row[1]
 # end
 
-CSV.open('example.csv', 'w+')  do |csv_object|
-  customers.each do |row|
-    csv_object << row
-  end
+# customers.each do |customer|
+#  customer << average_money_spent.shift
+# end
+
+customers.each do |row|
+  puts row.inspect
 end
+
+# CSV.open('example.csv', 'w+')  do |csv_object|
+#   customers.each do |row|
+#     csv_object << row
+#   end
+# end
